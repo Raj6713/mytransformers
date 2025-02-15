bd5e1b6cded17f9b354b4dc2e47a5fd51644e0be
raj6713631 <raj6713631@gmail.com>
2025-02-15 23:11:11 +0530

    Detr Loss functions added

--- src/transfomers/activations.py
+++ src/transfomers/activations.py
@@ -7,63 +7,94 @@ from .utils import logging
 
 logger = logging.get_logger(__name__)
 
+
 class PytorchGELUTanh(nn.Module):
     def __init__(self):
         super().__init__()
         if version.parse(torch.__version__) < version.parse("1.12.0"):
-            raise ImportError(f"You are using torch=={torch.__version__} but torch>=1.12.0 is required to use "
-                              "PytorchGELUTanh. Please upgrade torch.")
-        
-    def forward(self, input:Tensor) -> Tensor:
+            raise ImportError(
+                f"You are using torch=={torch.__version__} but torch>=1.12.0 is required to use "
+                "PytorchGELUTanh. Please upgrade torch."
+            )
+
+    def forward(self, input: Tensor) -> Tensor:
         return nn.functional.gelu(input, approximate="tanh")
-    
+
+
 class NewGELUActivation(nn.Module):
-    def forward(self, input:Tensor) -> Tensor:
-        return 0.5 * input *(1.0 + torch.tanh(math.sqrt(2.0/math.pi) *(input + 0.044715 * torch.pow(input, 3.0))))
-    
+    def forward(self, input: Tensor) -> Tensor:
+        return (
+            0.5
+            * input
+            * (
+                1.0
+                + torch.tanh(
+                    math.sqrt(2.0 / math.pi)
+                    * (input + 0.044715 * torch.pow(input, 3.0))
+                )
+            )
+        )
+
+
 class GELUActivation(nn.Module):
-    def __init__(self, use_gelu_python:bool=False):
+    def __init__(self, use_gelu_python: bool = False):
         super().__init__()
         if use_gelu_python:
             self.act = self._gelu_python
         else:
             self.act = nn.functional.gelu
-    
-    def _gelu_python(self, input:Tensor) -> Tensor:
-        return 0.5 * input *(1.0 + torch.erf(input/math.sqrt(2.0)))
-    
-    def forward(self, input:Tensor) -> Tensor:
+
+    def _gelu_python(self, input: Tensor) -> Tensor:
+        return 0.5 * input * (1.0 + torch.erf(input / math.sqrt(2.0)))
+
+    def forward(self, input: Tensor) -> Tensor:
         return self.act(input)
-    
+
+
 class FastGELUActivation(nn.Module):
-    def forward(self, input:Tensor) -> Tensor:
-        return 0.5 * input * (1.0 + torch.tanh(input*0.79788) * (1.0 +0.044715*input * input))
-    
+    def forward(self, input: Tensor) -> Tensor:
+        return (
+            0.5
+            * input
+            * (1.0 + torch.tanh(input * 0.79788) * (1.0 + 0.044715 * input * input))
+        )
+
 
 class QuickGELUActivation(nn.Module):
-    def forward(self, input:Tensor) -> Tensor:
-        return input * torch.sigmoid(1.702*input)
-    
+    def forward(self, input: Tensor) -> Tensor:
+        return input * torch.sigmoid(1.702 * input)
+
 
 class ClippedGELUActivation(nn.Module):
-    def __init__(self, min:float, max:float):
+    def __init__(self, min: float, max: float):
         if min > max:
             raise ValueError("min should be < max (got min: {min}, max: {max})")
         super().__init__()
         self.min = min
         self.max = max
 
-    def forward(self, x:Tensor) -> Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         return torch.clip(gelu(x), self.min, self.max)
+
 
 class AccurateGELUActivation(nn.Module):
     def __init__(self):
         super().__init__()
-        self.precomputed_constant = math.sqrt(2/math.pi)
+        self.precomputed_constant = math.sqrt(2 / math.pi)
 
-    def forward(self, input:Tensor) -> Tensor:
-        return 0.5*input *(1+torch.tanh(self.precomputed_constant*(input+0.044715*torch.pow(input,3))))
-    
+    def forward(self, input: Tensor) -> Tensor:
+        return (
+            0.5
+            * input
+            * (
+                1
+                + torch.tanh(
+                    self.precomputed_constant * (input + 0.044715 * torch.pow(input, 3))
+                )
+            )
+        )
+
+
 class MishActivation(nn.Module):
     def __init__(self):
         super().__init__()
@@ -71,28 +102,33 @@ class MishActivation(nn.Module):
             self.act = self._mish_python
         else:
             self.act = nn.functional.mish
-    
-    def _mish_python(self, input:Tensor) -> Tensor:
+
+    def _mish_python(self, input: Tensor) -> Tensor:
         return input * torch.tanh(nn.functional.softplus(input))
-    
-    def forward(self, input:Tensor)->Tensor:
+
+    def forward(self, input: Tensor) -> Tensor:
         return self.act(input)
-    
+
+
 class LaplaceActivation(nn.Module):
-    def forward(self, input,mu=0.707107, sigma=0.282095):
-        input = (input-mu).div(sigma * math.sqrt(2.0))
-        return 0.5 *(1.0 + torch.erf(input))
+    def forward(self, input, mu=0.707107, sigma=0.282095):
+        input = (input - mu).div(sigma * math.sqrt(2.0))
+        return 0.5 * (1.0 + torch.erf(input))
+
+
 class ReLUSquareActivation(nn.Module):
     def forward(self, input):
         relu_applied = nn.functional.relu(input)
         squared = torch.square(relu_applied)
         return squared
-    
+
+
 class ClassInstantier(OrderedDict):
     def __getitem__(self, key):
         content = super().__getitem__(key)
         cls, kwargs = content if isinstance(content, tuple) else (content, {})
         return cls(**kwargs)
+
 
 class LinearActivation(nn.Module):
     """
@@ -101,10 +137,11 @@ class LinearActivation(nn.Module):
 
     def forward(self, input: Tensor) -> Tensor:
         return input
-    
+
+
 ACT2CLS = {
     "gelu": GELUActivation,
-    "gelu_10": (ClippedGELUActivation, {"min": -10, "max":10}),
+    "gelu_10": (ClippedGELUActivation, {"min": -10, "max": 10}),
     "gelu_fast": FastGELUActivation,
     "gelu_new": NewGELUActivation,
     "gelu_python": (GELUActivation, {"use_gelu_python": True}),
@@ -122,13 +159,18 @@ ACT2CLS = {
     "silu": nn.SiLU,
     "swish": nn.SiLU,
     "tanh": nn.Tanh,
-    "prelu": nn.PReLU
+    "prelu": nn.PReLU,
 }
 ACT2FN = ClassInstantier(ACT2CLS)
+
+
 def get_activation(activation_string):
     if activation_string in ACT2FN:
         return ACT2FN[activation_string]
     else:
-        raise KeyError(f"function {activation_string} not found in ACT2FN mapping {list(ACT2FN.keys())}")
-    
+        raise KeyError(
+            f"function {activation_string} not found in ACT2FN mapping {list(ACT2FN.keys())}"
+        )
+
+
 gelu_python = get_activation("gelu_python")
--- src/transfomers/cache_utils.py
+++ src/transfomers/cache_utils.py
@@ -8,7 +8,12 @@ import torch
 from packaging import version
 
 from .configuration_utils import PretrainedConfig
-from .utils import is_hqq_available, is_optimum_quanto_available, is_torchdynamo_compiling, logging
+from .utils import (
+    is_hqq_available,
+    is_optimum_quanto_available,
+    is_torchdynamo_compiling,
+    logging,
+)
 from .utils.deprecation import deprecate_kwarg
 
 if is_hqq_available():
@@ -16,12 +21,20 @@ if is_hqq_available():
 
 logger = logging.get_logger(__name__)
 
+
 class Cache(torch.nn.Module):
     is_compileable = False
 
     def __init__(self):
         super().__init__()
-    def update(self, key_states:torch.Tensor,value_states:torch.Tensor, layer_idx:int, cache_kwargs:Optional[Dict[str,Any]]=None) -> Tuple[torch.Tensor, torch.Tensor]:
+
+    def update(
+        self,
+        key_states: torch.Tensor,
+        value_states: torch.Tensor,
+        layer_idx: int,
+        cache_kwargs: Optional[Dict[str, Any]] = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
         """_summary_
 
         Args:
@@ -34,8 +47,8 @@ class Cache(torch.nn.Module):
             Tuple[torch.Tensor, torch.Tensor]: _description_
         """
         raise NotImplementedError("Make sure to implement `update` in a subclass.")
-    
-    def get_seq_length(self, layer_idx:Optional[int]=0) -> int:
+
+    def get_seq_length(self, layer_idx: Optional[int] = 0) -> int:
         """_summary_
 
         Args:
@@ -45,31 +58,39 @@ class Cache(torch.nn.Module):
             int: _description_
         """
         raise NotImplementedError("Make sure to implement `seq_length` in a subclass.")
-    
-    def get_max_cache_shape(self)-> Optional[int]:
+
+    def get_max_cache_shape(self) -> Optional[int]:
         """_summary_
 
         Returns:
             Optional[int]: _description_
         """
-        raise NotImplementedError("Make sure to implement `max_cache_shape` in a subclass.")
-    
-    def get_usable_length(self, new_seq_length:int, layer_idx:Optional[int]=0) -> int:
+        raise NotImplementedError(
+            "Make sure to implement `max_cache_shape` in a subclass."
+        )
+
+    def get_usable_length(
+        self, new_seq_length: int, layer_idx: Optional[int] = 0
+    ) -> int:
         max_length = self.get_max_cache_shape()
         previous_seq_length = self.get_seq_length(layer_idx)
-        if max_length is not None and previous_seq_length +new_seq_length > max_length:
-            return max_length -new_seq_length
+        if max_length is not None and previous_seq_length + new_seq_length > max_length:
+            return max_length - new_seq_length
         return previous_seq_length
 
-    def reorder_cache(self, beam_idx:torch.LongTensor):
+    def reorder_cache(self, beam_idx: torch.LongTensor):
         for layer_idx in range(len(self.key_cache)):
             if self.key_cache[layer_idx] != []:
                 device = self.key_cache[layer_idx].device
-                self.key_cache[layer_idx] = self.key_cache[layer_idx].index_select(0, beam_idx.to(device))
+                self.key_cache[layer_idx] = self.key_cache[layer_idx].index_select(
+                    0, beam_idx.to(device)
+                )
             if self.value_cache[layer_idx] != []:
                 device = self.value_cache[layer_idx].device
-                self.value_cache[layer_idx] = self.value_cache[layer_idx].index_select(0, beam_idx.to(device))
-    
+                self.value_cache[layer_idx] = self.value_cache[layer_idx].index_select(
+                    0, beam_idx.to(device)
+                )
+
     @property
     def seen_tokens(self):
         logger.warning_once(
@@ -80,11 +101,12 @@ class Cache(torch.nn.Module):
             return self._seen_tokens
         else:
             return None
-        
+
+
 @dataclass
 class CacheConfig:
     cache_implementation: None
-    
+
     @classmethod
     def from_dict(cls, config_dict, **kwargs):
         """_summary_
@@ -92,17 +114,17 @@ class CacheConfig:
         Args:
             config_dict (_type_): _description_
         """
-        config = cls(**config_dict) 
+        config = cls(**config_dict)
         to_remove = []
         for key, value in kwargs.items():
             if hasattr(config, key):
-                setattr(config, key,value)
+                setattr(config, key, value)
                 to_remove.append(key)
         for key in to_remove:
             kwargs.pop(key, None)
         return config
-    
-    def to_json_file(self, json_file_path:Union[str, os.PathLike]):
+
+    def to_json_file(self, json_file_path: Union[str, os.PathLike]):
         """_summary_
 
         Args:
@@ -112,11 +134,338 @@ class CacheConfig:
             config_dict = self.to_dict()
             json_string = json.dumps(config_dict, indent=2, sort_keys=True) + "\n"
             writer.write(json_string)
-    
+
     def to_dict(self) -> Dict[str, Any]:
         return copy.deepcopy(self.__dict__)
-    
+
     def __iter__(self):
         for attr, value in copy.deepcopy(self.__dict__).items():
             yield attr, value
-            
+
+    def __repr__(self):
+        for attr, value in copy.deepcopy(self.__dict__).items():
+            return f"{self.__class__.__name__} {self.to_json_string()}"
+
+    def to_json_string(self):
+        return json.dumps(self.__dict__, indent=2) + "\n"
+
+    def update(self, **kwargs):
+        """_summary_"""
+        to_remove = []
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+                to_remove.append(key)
+        usused_kwargs = {
+            key: value for key, value in kwargs.items() if key not in to_remove
+        }
+        return usused_kwargs
+
+
+@dataclass
+class QuantizedCacheConfig(CacheConfig):
+    """
+    Configuration class for quantized cache settings.
+
+    Attributes:
+        backend (`str`, *optional*, defaults to `"quanto"`):
+            Backend to use when performing quantization, Can be one of [`quanto`, `HQQ`]
+        nbits (`Optional[int]`, *optional*, defaults to 4):
+            Number of bits, can be 2 or 4 for the `quanto` backend and one of [1, 2, 3, 4, 8] for the `HQQ` backend. Defaults to 2.
+        axis_key (`int`, *optional*, defaults to 0):
+            Axis over which to perform grouping for the key tensors. Can be [0, -1] for `quanto` backend and [0, 1] for `HQQ` backend.
+        axis_value (`int`, *optional*, defaults to 0):
+            Axis over which to perform grouping for the value tensors. Can be [0, -1] for `quanto` backend and [0, 1] for `HQQ` backend.
+        q_group_size (`Optional[int]`, *optional*, defaults to 64):
+            Size of the quantization group, should be a divisor of the model's hidden dimension.
+            Defaults to 64.
+        residual_length (`Optional[int]`, *optional*, defaults to 128):
+            Length of the residual cache which will always be stored in original presicion.
+            Defaults to 128.
+        compute_dtype (`torch.dtype`, *optional*, defaults to `torch.float16`):
+            The defualt dtype used for computations in the model. Keys and Values will be cast to this dtype after dequantization.
+        device (`str`, *optional*, defaults to `"cpu"`):
+            Device on which to perform computations, should be same as the model's device.
+    """
+
+    def __init__(
+        self,
+        backend: str = "quanto",
+        nbits: Optional[int] = 4,
+        axis_key: Optional[int] = 0,
+        axis_value: Optional[int] = 0,
+        q_group_size: Optional[int] = 64,
+        residual_length: Optional[int] = 128,
+        compute_dtype: Optional[torch.dtype] = torch.float16,
+        device: Optional[str] = "cpu",
+    ):
+        self.backend = backend
+        self.nbits = nbits
+        self.axis_key = axis_key
+        self.axis_value = axis_value
+        self.q_group_size = q_group_size
+        self.residual_length = residual_length
+        self.compute_dtype = compute_dtype
+        self.device = device
+
+    def validate(self):
+        """validate if the argumnets passed are correct"""
+        incorrect_arg_msg = (
+            "Some of the key in `cache_config` are defined incorrectly. `{key}` should be `{correct_value}`"
+            "but found {found_value}"
+        )
+        if self.nbits not in [1, 2, 3, 4, 8]:
+            raise ValueError(
+                incorrect_arg_msg.format(
+                    key="nbits", correct_value="2 or 4 or 8", found_value=self.nbits
+                )
+            )
+        if self.q_group_size <= 0:
+            raise ValueError(
+                incorrect_arg_msg.format(
+                    key="q_group_size",
+                    correct_size="a positive integer",
+                    found_value=self.q_group_size,
+                )
+            )
+        if self.residual_length < 0:
+            raise ValueError(
+                incorrect_arg_msg.format(
+                    key="residual_length",
+                    correct_value="a positive length",
+                    found_value=self.residual_length,
+                )
+            )
+        if self.axis_key not in [0, 1, -1]:
+            raise ValueError(
+                incorrect_arg_msg.format(
+                    key="axis_key",
+                    correct_value="value should be in [0,1,-1]",
+                    found_value=self.axis_key,
+                )
+            )
+        if self.axis_value not in [0, 1, -1]:
+            raise ValueError(
+                incorrect_arg_msg.format(
+                    key="axis_value", correct_value="value should be in `0`, `1`, `-1`"
+                )
+            )
+
+
+@dataclass
+class StaticCacheConfig(CacheConfig):
+    """Configuration class for static cache settings
+
+    Args:
+        CacheConfig (_type_): _description_
+    """
+
+    cache_implementation = "static"
+
+    def __init__(self, batch_size: int, max_cache_len: int, device="cpu"):
+        self.batch_size = batch_size
+        self.max_cache_len = max_cache_len
+        self.device = device
+
+    def validate(self):
+        """Validates if the the arguments passed are correct"""
+        incorrect_arg_msg = (
+            "Some of the keys in `cache_config` are defined incorrectly. `{key}` should be `{correct_value}`"
+            " but found {found_value}"
+        )
+        if self.batch_size < 0:
+            raise ValueError(
+                key="batch_size", correct_value=" > 0", found_values=self.batch_size
+            )
+        if self.max_cache_len <= 0:
+            raise ValueError(
+                key="max_cache_len", correct_value="> 0", found_value=self.max_cache_len
+            )
+
+
+class DynamicCache(Cache):
+    """
+    A cache that grows dynamically as more tokens are generated. This is the default for generative models.
+
+    It stores the Key and Value states as a list of tensors, one for each layer. The expected shape for each tensor is
+    `[batch_size, num_heads, seq_len, head_dim]`.
+
+    Example:
+
+        ```python
+        >>> from transformers import AutoTokenizer, AutoModelForCausalLM, DynamicCache
+
+        >>> model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-0.5B-Instruct")
+        >>> tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B-Instruct")
+
+        >>> inputs = tokenizer(text="My name is Qwen2", return_tensors="pt")
+
+        >>> # Prepare a cache class and pass it to model's forward
+        >>> past_key_values = DynamicCache()
+        >>> outputs = model(**inputs, past_key_values=past_key_values, use_cache=True)
+        >>> outputs.past_key_values # access cache filled with key/values from generation
+        DynamicCache()
+        ```
+    """
+
+    @deprecate_kwarg("num_hidden_layers", version="4.47.0")
+    def __init__(self, num_hidden_layers: Optional[int] = None) -> None:
+        super().__init__()
+        self._seen_tokens = 0
+        self.key_cache: List[torch.Tensor] = []
+        self.value_cache: List[torch.Tensor] = []
+
+    def __getitem__(self, layer_idx: int) -> List[Tuple[torch.Tensor]]:
+        """Support for backward compatibility
+
+        Args:
+            layer_idx (int): _description_
+
+        Returns:
+            List[Tuple[torch.Tensor]]: _description_
+        """
+        if layer_idx < len(self):
+            return (self.key_cache[layer_idx], self.value_cache[layer_idx])
+        else:
+            raise KeyError(
+                f"Cache only has {len(self)} layers, attempted to access layer with index {layer_idx}"
+            )
+
+    def __iter__(self):
+        """Support for the backward compatibilty"""
+        for layer_idx in range(len(self)):
+            yield (self.key_cache[layer_idx], self.value_cache[layer_idx])
+
+    def __len__(self):
+        return len(self.key_cache)
+
+    def update(
+        self,
+        key_states: torch.Tensor,
+        value_states: torch.Tensor,
+        layer_idx: int,
+        cache_kwargs: Optional[Dict[str, Any]] = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        if layer_idx == 0:
+            self._seen_tokens += key_states.shape[-2]
+        if key_states is not None:
+            if len(self.key_cache) <= layer_idx:
+                for _ in range(len(self.key_cache), layer_idx):
+                    self.key_cache.append([])
+                    self.value_cache.append([])
+                self.key_cache.append(key_states)
+                self.value_cache.append(value_states)
+            elif len(self.key_cache[layer_idx]) == 0:
+                self.key_cache[layer_idx] = key_states
+                self.value_cache[layer_idx] = value_states
+            else:
+                self.key_cache[layer_idx] = torch.cat(
+                    [self.key_cache[layer_idx], key_states], dim=-2
+                )
+                self.value_cache[layer_idx] = torch.cat(
+                    [self.value_cache[layer_idx], value_states], dim=-2
+                )
+        return self.key_cache[layer_idx], self.value_cache[layer_idx]
+
+    def get_seq_length(self, layer_idx: Optional[int] = 0) -> int:
+        is_empty_layer = (
+            len(self.key_cache) == 0
+            or len(self.key_cache) <= layer_idx
+            or len(self.key_cache[layer_idx]) == 0
+        )
+        layer_seq_length = (
+            self.key_cache[layer_idx].shape[-2] if not is_empty_layer else 0
+        )
+        return layer_seq_length
+
+    def get_max_cache_shape(self) -> Optional[int]:
+        return None
+
+    def to_legacy_cache(self) -> Tuple[Tuple[torch.Tensor], Tuple[torch.Tensor]]:
+        legacy_cache = {}
+        for layer_idx in range(len(self)):
+            legacy_cache += (self.key_cache[layer_idx], self.value_cache[layer_idx])
+        return legacy_cache
+
+    @classmethod
+    @deprecate_kwarg("num_hidden_layer", version="4.47.0")
+    def from_legacy_cache(
+        cls,
+        past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
+        num_hidden_layers: int = None,
+    ) -> "DynamicCache":
+        cache = cls()
+        if past_key_values is not None:
+            for layer_idx in range(len(past_key_values)):
+                key_states, value_states = past_key_values[layer_idx]
+                cache.update(key_states, value_states, layer_idx)
+        return cache
+
+    def crop(self, max_length: int):
+        if max_length < 0:
+            max_length = self.get_seq_length() - abs(max_length)
+        if self.get_seq_length() <= max_length:
+            return
+        self._seen_tokens = max_length
+        for idx in range(len(self.key_cache)):
+            if self.key_cache[idx] != []:
+                self.key_cache[idx] = self.key_cache[idx][..., :max_length, :]
+                self.value_cache[idx] = self.value_cache[idx][..., :max_length, :]
+
+    @deprecate_kwarg("num_hidden_layers", version="4.47.0")
+    def batch_split(
+        self, full_batch_size: int, split_size: int, num_hidden_layers: int = None
+    ) -> List["DynamicCache"]:
+        out = []
+        for i in range(0, full_batch_size, split_size):
+            current_split = DynamicCache()
+            current_split._seen_tokens = self._seen_tokens
+            current_split.key_cache = [
+                tensor[i : i + split_size] for tensor in self.key_cache
+            ]
+            current_split.value_cache = [
+                tensor[i : i + split_size] for tensor in self.value_cache
+            ]
+            out.append(current_split)
+        return out
+
+    @classmethod
+    @deprecate_kwarg("num_hidden_layers", version="4.47.0")
+    def from_batch_splits(
+        cls, splits: List["DynamicCache"], num_hidden_layers: int = None
+    ) -> "DynamicCache":
+        cache = cls()
+        for idx in range(len(splits[0])):
+            key_cache = [
+                current.key_cache[idx]
+                for current in splits
+                if current.key_cache[idx] != []
+            ]
+            value_cache = [
+                current.value_cache[idx]
+                for current in splits
+                if current.value_cache != []
+            ]
+            if key_cache != []:
+                layer_keys = torch.cat(key_cache, dim=0)
+                layer_values = torch.cat(value_cache, dim=0)
+                cache.update(layer_keys, layer_values, idx)
+        return cache
+
+    def batch_repeat_interleave(self, repeats: int):
+        for layer_idx in range(len(self)):
+            self.key_cache[layer_idx] = self.key_cache[layer_idx].repeat_interleave(
+                repeats, dim=0
+            )
+            self.value_cache[layer_idx] = self.value_cache[layer_idx].repeat_interleave(
+                repeats, dim=0
+            )
+
+    def batch_select_indices(self, indices: torch.Tensor):
+        for layer_idx in range(len(self)):
+            self.key_cache[layer_idx] = self.key_cache[layer_idx][indices, ...]
+            self.value_cache[layer_idx] = self.value_cache[layer_idx][indices, ...]
+
+
+# Done till line no 540
+#
