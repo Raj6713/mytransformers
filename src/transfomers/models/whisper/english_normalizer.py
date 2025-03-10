3a378fee365a504621e647025a75dd9a243b2862
raj6713631 <raj6713631@gmail.com>
2025-03-10 16:47:07 +0530

    feature extraction added

---
+++ src/transfomers/models/whisper/english_normalizer.py
@@ -0,0 +1,101 @@
+import re
+import unicodedata
+from fractions import Fraction
+from typing import Iterator, List, Match, Optional, Union
+import regex
+ADDITIONAL_DIACRITICS = {
+    "œ": "oe",
+    "Œ": "OE",
+    "ø": "o",
+    "Ø": "O",
+    "æ": "ae",
+    "Æ": "AE",
+    "ß": "ss",
+    "ẞ": "SS",
+    "đ": "d",
+    "Đ": "D",
+    "ð": "d",
+    "Ð": "D",
+    "þ": "th",
+    "Þ": "th",
+    "ł": "l",
+    "Ł": "L",
+}
+
+def remove_symbols_and_diacritics(s:str, keep=""):
+    def replace_character(char):
+        if char in keep:
+            return char
+        elif char in ADDITIONAL_DIACRITICS:
+            return ADDITIONAL_DIACRITICS[char]
+        elif unicodedata.category(char) == "Mn":
+            return ""
+        elif unicodedata.category(char)[0] in "MSP":
+            return " "
+        return char
+    
+    return "".join(replace_character(c) for c in unicodedata.normalize("NFKD", s))
+
+def remove_symbols(s:str):
+    return "".join(" " if unicodedata.category(c)[0] in "MSP" else c for c in unicodedata.normalize("NFKC", s))
+
+class BasicTextNormalizer:
+    def __init__(self, remove_diacritics:bool=False, split_letters:bool=False):
+        self.clean = remove_symbols_and_diacritics if remove_diacritics else remove_symbols
+        self.split_letters = split_letters
+
+    def __call__(self, s:str):
+        s = s.lower()
+        s = re.sub(r"[<\[][^>\]]*[>\]]", "", s)  # remove words between brackets
+        s = re.sub(r"\(([^)]+?)\)", "", s)  # remove words between parenthesis
+        s = self.clean(s).lower()
+        if self.split_letters:
+            s = " ".join(regex.findall(f"\X", s, regex.U))
+        s = re.sub(r"\s+", " ", s)
+        return s
+
+class EnglishNumberNormalizer:
+    """ Convert any spelled-out numbers into arabic numbers, while handling
+        - remove any commas
+        - keep the suffixes such as `1960s`
+        - spell out currency symbols after the number
+        - spell out `one` and `ones`
+        - interpret successive single digit numbers as nominal    
+    """
+    def __init__(self):
+        super().__init__()
+        self.zeros = {"o","oh", "zero"}
+        self.ones = {
+            name: i for i, name in enumerate(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],start=1)
+        }
+        self.ones_plural = {"sixes" if name == "six" else name+"s": (value,"s") for name, value in self.ones.items()}
+        self.ones_ordinal = {
+            "zeroth": (0, "th"),
+            "first": (1, "st"),
+            "second": (2, "nd"),
+            "third": (3, "rd"),
+            "fifth": (5, "th"),
+            "twelfth": (12, "th"),
+            **{ name + ("h" if name.endswith("t") else "th"): (value, "th") for name, value in self.ones.items() if value > 3 and value != 5 and value != 12}
+        }
+        self.ones_suffixed = {**self.ones_plural, **self.ones_ordinal}
+        self.tens = {"twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "sevenety": 70, "eighty": 80, "ninety": 90}
+        self.tens_plural = {name.replace("y", "ies"):(value, "s") for name, value in self.tens.items()}
+        self.tens_ordinal = {name.replace("y", "ieth"): (value, "th") for name, value in self.tens.items()}
+        self.tens_suffixed = {**self.tens_plural, **self.tens_ordinal}
+        self.multipliers = {
+            "hundred": 100,
+            "thousand": 1_000,
+            "million": 1_000_000,
+            "billion": 1_000_000_000,
+            "trillion": 1_000_000_000_000,
+            "quadrillion": 1_000_000_000_000_000,
+            "quintillion": 1_000_000_000_000_000_000,
+            "sextillion": 1_000_000_000_000_000_000_000,
+            "septillion": 1_000_000_000_000_000_000_000_000,
+            "octillion": 1_000_000_000_000_000_000_000_000_000,
+            "nonillion": 1_000_000_000_000_000_000_000_000_000_000,
+            "decillion": 1_000_000_000_000_000_000_000_000_000_000_000,
+        }
+        
+
